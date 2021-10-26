c1d217538579e029bc440fdf43ae2da6256c32b7
Dulvin Witharane <dulvinw@gmail.com>
2021-10-26 11:29:55 +0530

    Better sublist solution with windows

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -7,8 +7,16 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    if _first_list.len() == 0 && _second_list.len() == 0 {
+    if _first_list == _second_list {
         return Comparison::Equal;
+    }
+
+    if _first_list == [] {
+        return Comparison::Sublist;
+    }
+
+    if _second_list == [] {
+        return Comparison::Superlist;
     }
 
     if _first_list.len() > _second_list.len() {
@@ -19,24 +27,9 @@ pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Compariso
 }
 
 fn sublist_impl<T: PartialEq>(_first_list: &[T], _second_list: &[T], set_type: Comparison) -> Comparison {
-    let len = _second_list.len() - _first_list.len() + 1;
-    for i in 0..len {
-        let mut found: bool = true;
-        let mut first_list_index: usize = 0;
-
-        while first_list_index < _first_list.len() {
-            if _first_list[first_list_index] != _second_list[first_list_index+i] {
-                found = false;
-                break;
-            }
-            first_list_index += 1;
-        }
-        if found && _first_list.len() != _second_list.len() {
+    for sublist in _second_list.windows(_first_list.len()) {
+        if sublist == _first_list {
             return set_type;
-        }
-
-        if found {
-            return Comparison::Equal;
         }
     }
 
